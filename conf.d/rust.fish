ccf55786b723c185b2b46423c195e261e19c3b88
kpbaks <kristoffer.pbs@tuta.io>
2024-01-16 12:11:31 +0100

    fix(__complete_crates.io): fix test for $limit being between 1 and 100

--- conf.d/rust.fish
+++ conf.d/rust.fish
@@ -175,18 +175,23 @@ abbr -a cgwt cargo watch $cargo_watch_flags --exec test
 set -l c complete -c cargo
 
 function __complete_crates.io
-    set -l limit 20
-    test 1 -gt $limit -a $limit -le 100; or return 1 # Limit imposed by crates.io
-    set -l query (commandline --current-token)
+    set -l limit 35
+    test $limit -gt 1 -a $limit -le 100; or return 1 # Limit imposed by crates.io
+    if test (count $argv) -eq 1
+        set -f query $argv[1]
+    else
+        set -f query (commandline --current-token)
+    end
     test -n $query; or return 0 # Nothing to search for
     test (string sub --length=1 -- $query) = -; and return 0 # The token at the cursor is a some kind of option. `cargo search` will be confused by that
 
     set -l regexp (printf "^(%s[\w_-]+) = \"(\d+\.\d+\.\d+)\"\s+#\s(.+)\$" $query)
 
+    # PERF: add a caching mechanism for doing the same query 2 or more times consequtively
     command cargo search --limit=$limit $query \
         | string match --regex --groups-only -- $regexp \
         | while read --line crate_name crate_version desc
-        printf "%s\t(%s) %s\n" $crate_name $crate_version $desc
+        printf "%s\t(v%s) %s\n" $crate_name $crate_version $desc
         # printf "%s\t%s\n" $crate_name $desc
     end
 end
